8db3beca3a960717a43218227bcf296931f0a085
120296Soumyaju <logic.codedebugrepeat@gmail.com>
2025-08-26 23:59:23 +0530

    Fix import path for health check view and corsheadher vercel / removed

--- job-portal-backend/job_portal/settings.py
+++ job-portal-backend/job_portal/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://job-portal-webapp-iota.vercel.app/",
+    "https://job-portal-webapp-iota.vercel.app",
 
 
 ]
--- job-portal-backend/job_portal/urls.py
+++ job-portal-backend/job_portal/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from jobs.views import home  # import the home view
+from jobs.views import home,health_check  # import the home view
 
 
 schema_view = get_schema_view(
