78e4263bf538bf875bda53ad52a52110105d52c7
120296Soumyaju <logic.codedebugrepeat@gmail.com>
2025-08-26 16:32:58 +0530

    Updated backend settings for NeonDB

--- job-portal-backend/job_portal/settings.py
+++ job-portal-backend/job_portal/settings.py
@@ -69,6 +69,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',   # <-- Needed for Neon
+        },
     }
 }
 
